3ea2582863d35d54fc28308a676460d302abbb74
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2021-01-03 15:06:32 -0800

    should be working now

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Post
+from .serializers import PostSerializer
+from .permissions import IsAuthorized
 
-# Create your views here.
+
+class CreatePost(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class RetrieveUpdateDestroyPost(RetrieveUpdateDestroyAPIView):
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    #ADDED TO AUTH USER
+    permission_classes = [IsAuthorized]
